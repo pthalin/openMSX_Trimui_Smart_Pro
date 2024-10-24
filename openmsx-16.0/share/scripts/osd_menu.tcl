270602e03ad3e8f50ad75e9d6bf4c74698571f2b
pthalin <patrik@thalin.se>
2024-10-24 05:39:43 +0200

    Adjusted menu title color

--- openmsx-16.0/share/scripts/osd_menu.tcl
+++ openmsx-16.0/share/scripts/osd_menu.tcl
@@ -4,7 +4,7 @@ namespace eval osd_menu {
 variable default_bg_color "0x7090aae8 0xa0c0dde8 0x90b0cce8 0xc0e0ffe8"
 variable default_text_color 0x000000ff
 variable default_select_color "0x0044aa80 0x2266dd80 0x0055cc80 0x44aaff80"
-variable default_header_text_color 0xff9020ff
+variable default_header_text_color 0xe04000ff
 
 # drop handler
 bind_default filedrop osd_menu::drop_handler -event
