835dd3d437c5f4c94e8c454b12f39fd6a93f6af5
pthalin <patrik@thalin.se>
2024-11-17 21:09:55 +0100

    Add support script

---
+++ crossmix/MSX2/openmsx_support.sh
@@ -0,0 +1,1 @@
+ /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i "/mnt/SDCARD/Emus/MSX2/openmsx/resources/background.png" -c "0,0,0" -f "/mnt/SDCARD/System/resources/DejaVuSans.ttf" -m "Visit: github.com/pthalin" -k " "
