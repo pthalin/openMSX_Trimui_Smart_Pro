aa66eafd2015948b7752d3206950d2bb10d5e897
pthalin <patrik@thalin.se>
2024-11-18 19:26:31 +0100

    Improved scripts

--- crossmix/MSX2/openmsx_msx2plus.sh
+++ crossmix/MSX2/openmsx_msx2plus.sh
@@ -11,6 +11,7 @@ export OPENMSX_SYSTEM_DATA=$PWD/share
 export HOME=$EMU_DIR
 
 SERVER_URL="https://download.file-hunter.com/System%20ROMs/machines/panasonic"
+AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"
 
 SYS_ROMS=" fs-a1wsx_basic-bios2p.rom
  fs-a1wsx_disk.rom
@@ -26,7 +27,11 @@ do
     echo "Found sysrom $f"
   else
     echo "Downloading sysrom $f " 
-    curl --fail-early --connect-timeout 10 -k -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" "$SERVER_URL/$f" -o ./share/systemroms/$f
+    curl --fail-early --connect-timeout 10 -k -A "$AGENT" "$SERVER_URL/$f" -o ./share/systemroms/$f
+    if [ ! -f "./share/systemroms/$f" ]; then
+      ../openmsx_download_error.sh
+      exit
+    fi
   fi
 done
 
--- crossmix/MSX2/openmsx_support.sh
+++ crossmix/MSX2/openmsx_support.sh
@@ -1,1 +1,7 @@
- /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i "/mnt/SDCARD/Emus/MSX2/openmsx/resources/background.png" -c "0,0,0" -f "/mnt/SDCARD/System/resources/DejaVuSans.ttf" -m "Visit: github.com/pthalin" -fs 50 -k " "
+ /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh \
+     -i "/mnt/SDCARD/Emus/MSX2/openmsx/resources/background.png" \
+     -c "0,0,0" \
+     -f "/mnt/SDCARD/System/resources/DejaVuSans.ttf" \
+     -m "Visit: github.com/pthalin" \
+     -fs 50 \
+     -k " "
