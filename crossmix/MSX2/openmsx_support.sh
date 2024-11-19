2a323ae5cea2dc875fc9c593c2f610a6f9c168b1
pthalin <patrik@thalin.se>
2024-11-19 07:41:08 +0100

    Add Guide

--- crossmix/MSX2/openmsx_support.sh
+++ crossmix/MSX2/openmsx_support.sh
@@ -1,7 +1,7 @@
  /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh \
-     -i "/mnt/SDCARD/Emus/MSX2/openmsx/resources/background.png" \
+     -i "/mnt/SDCARD/Emus/MSX2/openmsx/resources/guide.png" \
      -c "0,0,0" \
      -f "/mnt/SDCARD/System/resources/DejaVuSans.ttf" \
-     -m "Visit: github.com/pthalin" \
+     -m "" \
      -fs 50 \
      -k " "
